89857f5962c3e2d64af4960bc887237b3847a018
lx <37569755+SunDarkCode@users.noreply.github.com>
2018-07-20 17:10:47 +0800

    fly_record
    记录ectouch二次开发 加入购物车商品飞入

---
+++ fly_cart.js
@@ -0,0 +1,27 @@
+﻿      <script src="js/jquery.fly.min.js"></script>
+      <script src="js/requestAnimationFrame.js"></script>
+	<script>
+		function addFly(event) {
+	    var qu=$(window).scrollTop();//页面滚动高度
+                     var img=$('#flyimg').attr('value');
+                     var flyera=$('<img class="u-flyer" src="'+img+'">');
+		    var offset = $('#total_number').offset(), flyer =flyera;
+		    flyer.fly({
+		        start: {
+		            left: event.pageX-40,
+		            top: event.pageY-20-qu
+		        },
+		        end: {
+		            left: offset.left-3,
+		            top: offset.top-qu,
+		            width: 1,
+		            height: 1,
+		        }
+		    });
+		    console.log($(window).scrollTop());
+		}
+		function as(event,img){
+			$('#flyimg').attr("value",img);
+			addFly(event);
+		}
+	</script>
